672817b57e234e58a875e09e255e8603955e8023
Robin-Hood-zjw <jiawenz@umich.edu>
2023-11-25 04:32:25 -0500

    install_mongoose_and_mongodb

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 /* eslint-disable import/newline-after-import */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './conflg.env' });
-
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+dotenv.config({ path: './conflg.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connections successfull');
+  });
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`App running port ${port}...`));
