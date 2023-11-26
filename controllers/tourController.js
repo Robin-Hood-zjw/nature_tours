e81680add198839ec25a9e506019fd6485a80fbc
Robin-Hood-zjw <jiawenz@umich.edu>
2023-11-26 06:07:05 -0500

    x002_rebundle_API_1

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -21,33 +10,32 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
+  res.status(200).json({ status: 'success', requestedAt: req.requestTime });
 };
 
 exports.getTour = (req, res) => {
-  const tour = tours.find((el) => el.id === req.params.id * 1);
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === req.params.id * 1);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tour },
+  // });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    }
-  );
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent.',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
