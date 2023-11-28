dbf1f1aef7fb88b39ad155118d60ef11cbd84268
Robin-Hood-zjw <jiawenz@umich.edu>
2023-11-28 01:40:03 -0500

    x002_tours_modeling_1

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,13 +3,24 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name'],
     unique: true,
+    required: [true, 'A tour must have a name'],
   },
   slug: String,
   duration: { type: Number, required: [true, 'A tour must have a duration'] },
-  rating: { type: Number, default: 4.5 },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: { type: Number, default: 4.5 },
+  ratingQuantity: { type: Number, default: 0 },
   price: { type: Number, required: [true, 'A tour must have a price'] },
+  priceDiscount: Number,
+  summary: { type: String, trim: true },
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
