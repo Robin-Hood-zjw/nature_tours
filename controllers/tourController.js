dfadc406639aff59c3b8f18e2b54af12ceef431e
Robin-Hood-zjw <jiawenz@umich.edu>
2023-12-03 05:03:41 -0500

    x002_API_aggregations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -71,3 +71,59 @@ exports.deleteTour = async (req, res) => {
     res.status(404).json({ status: 'fail', message: error });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $min: '$price' },
+        },
+      },
+      { $sort: { avgPrice: 1 } },
+    ]);
+
+    res.status(200).json({ status: 'success', data: { stats } });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      { $addFields: { month: '$_id' } },
+      { $project: { _id: 0 } },
+      { $sort: { numTourStarts: -1 } },
+      { $limit: 12 },
+    ]);
+
+    res.status(200).json({ status: 'success', data: { plan } });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
+};
