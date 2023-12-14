5d3879bb1009107f8660b09142578920dd3ffa91
Robin-Hood-zjw <jiawenz@umich.edu>
2023-12-14 02:05:01 -0500

    x002_build_API_aggregartion

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -38,9 +38,6 @@ exports.getTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    // const newTour = new Tour({})
-    // newTour.save()
-
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({ status: 'success', data: { tour: newTour } });
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -44,6 +45,16 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
