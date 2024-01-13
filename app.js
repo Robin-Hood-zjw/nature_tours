82233a95dcc78d713eb77dd776cc5a467f905202
Robin-Hood-zjw <jiawenz@umich.edu>
2024-01-13 17:25:09 -0500

    x004_err_manipulation

--- app.js
+++ app.js
@@ -19,4 +19,24 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
+});
+
 module.exports = app;
