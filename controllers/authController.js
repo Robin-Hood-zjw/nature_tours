8109ac87e8701d62c681f39a3f3cd4202aae6d40
Robin-Hood-zjw <jiawenz@umich.edu>
2024-01-22 22:17:30 -0500

    x004_user_permission_3

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,3 +71,15 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    if (!roles.includes(req.user.role))
+      return next(
+        new AppError('You do not have permission to perform this action'),
+        403,
+      );
+
+    next();
+  };
