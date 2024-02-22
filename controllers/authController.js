c756233ee2784f2149417d4c5548ba15acf5f420
Robin-Hood-zjw <jiawenz@umich.edu>
2024-02-22 17:33:30 -0500

    x004_fix_expression_version_error

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,10 +7,11 @@ const sendEmail = require('../utils/email');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-const signToken = (id) =>
+const signToken = (id) => {
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+};
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
