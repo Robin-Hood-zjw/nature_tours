6993dc8db620df6e039f1ef8e017e5971e13c4a9
Robin-Hood-zjw <jiawenz@umich.edu>
2024-02-25 23:34:57 -0500

    x004_login_tweak

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,15 +46,15 @@ exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   // 1) Check if email and password exist
-  if (!email || !password) {
+  if (!email || !password)
     return next(new AppError('Please provide email and password!', 400));
-  }
+
   // 2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select('+password');
+  const correct = await user.correctPassword(password, user.password);
 
-  if (!user || !(await user.correctPassword(password, user.password))) {
+  if (!user || !correct)
     return next(new AppError('Incorrect email or password', 401));
-  }
 
   // 3) If everything ok, send token to client
   createSendToken(user, 200, res);
